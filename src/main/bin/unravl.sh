80547950f313f609763e3076de89fc9647ab0512
David Biesack <David.Biesack@sas.com>
2016-10-05 18:56:41 -0400

    Clean up scripts for running in dev

--- src/main/bin/unravl.sh
+++ src/main/bin/unravl.sh
@@ -13,7 +13,7 @@ then # This works for a distribution, where UNRAVL is deployed in $UNRAVL_DIR,
      # the script is in $UNRAVL_DIR/bin,
      # and all the dependent jars are in  $UNRAVL_DIR/lib
      UNRAVL_CLASSPATH="$UNRAVL_DIR/lib/*:"
-elif [ -d $UNRAVL_DIR/../../build/libs -a -d $UNRAVL_DIR/../../build/output/lib ]
+elif [ -d $UNRAVL_DEV_DIR/build/classes -a -d $UNRAVL_DIR/../../build/output/lib ]
 then # This is for use in the build environment where the script
      # is in src/main/bin
      # Build with:
@@ -23,7 +23,7 @@ then # This is for use in the build environment where the script
      UNRAVL_JAR_DIR="$UNRAVL_DEV_DIR/build/libs"
      UNRAVL_LIB_DIR="$UNRAVL_DEV_DIR/build/output/lib"
      UNRAVL_CLASSPATH="$UNRAVL_LIB_DIR/*:$UNRAVL_JAR_DIR/*"
-else echo $DOADIE_DIR does not contain libraries.
+else echo $UNRAVL_DIR or $UNRAVL_DEV_DIR do not contain libraries
      echo If in development, run:
      echo   ./gradlew clean build copyDeps
      exit 1
